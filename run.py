08794e72a4e3a08462ec31f9b83a3efbfcf16562
Tanawatkub <67030091@kmitl.ac.th>
2025-10-31 01:12:34 +0700

    ww

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/api/health')
+@app.route("/api/health")
 def health():
     return jsonify({"status": "healthy"}), 200
 
-@app.route('/')
+@app.route("/")
 def home():
     return jsonify({"message": "Flask Todo CI/CD is running!"}), 200
 
-
 if __name__ == "__main__":
-    # ✅ Render จะส่งค่าพอร์ตจริงมาในตัวแปร PORT
-    port = int(os.environ.get("PORT", 5000))
+    # ✅ ใช้พอร์ตจาก Render
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
